99251ca90e05d3df1b36d9ebe5c7bf08ad3fc65e
gogvale <gabriel.oliveira@microplus.com.mx>
2018-10-01 12:26:30 -0500

    metodo de generacion de token reemplazado

--- SHELL/token.py
+++ SHELL/token.py
@@ -1,15 +1,44 @@
-#!python3
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[48]:
+
+
 import requests
+import os
+import getpass
 
-url = "https://10.4.29.121/api/"
 
-querystring = {"type":"keygen","user":"gogvale","password":"57%25fkObmcnYlI3XOcP7f"}
+# In[49]:
 
-headers = {
-    'Cache-Control': "no-cache",
-    'Postman-Token': "63c1fdd6-6792-4498-b423-72ee6a5e152e"
-    }
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+if __name__ == '__main__':
+    """
+    Programa utilizado para generar tokens de palo alto.
+    Generado por Postman
+    """
+    ip = input('IP de palo alto (e.g. 8.8.8.8):')
 
-print(response.text)
+    url = "https://{}/api/".format(ip)
+
+    querystring = {"type":"keygen"}
+    querystring["user"] = input('Usuario:')
+    querystring["password"] = getpass.getpass('Contraseña:')
+    
+    headers = {
+        'Cache-Control': "no-cache",
+        'Postman-Token': "63c1fdd6-6792-4498-b423-72ee6a5e152e"
+        }
+    response = requests.request("GET", url, headers=headers, params=querystring,verify=False)
+    xml = response.text
+    
+    if 'key' in xml:
+        print('Key found! Printing to hidden file...')
+        token = xml.split('key')[1].split('>')[1].split('<')[0]
+        with open(os.path.expanduser('~/NorsePi/SHELL/.tok.tmp'),'w') as file:
+            file.write(token)
+    else:
+        print('key not found. Check if username and password are correct and try again')
+        print('response:\n{}'.format(xml))
+    
+
