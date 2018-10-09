b5219700fadb4d3d645c1af230573c4470171c36
gogvale <gabriel.oliveira@microplus.com.mx>
2018-10-09 10:02:32 -0500

    funcion de corregir minutos agregada

--- SHELL/PaloAltoXML.py
+++ SHELL/PaloAltoXML.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[8]:
+# In[6]:
 
 
 from datetime import datetime, timedelta
@@ -14,9 +14,7 @@ import xmltodict
 import time
 
 
-# # Captura de Job
-
-# In[9]:
+# In[7]:
 
 
 def getJob(firewall, token, maxlogs, N=15):
@@ -44,23 +42,18 @@ def getJob(firewall, token, maxlogs, N=15):
     xml = response.text
 
     job = xml.split('line')[1].split()[-1].split('<')[0]
-
+    
     print('Finished.')
 
     print('#job:{}'.format(job))
-
+    
     return job
 
 
-# # Wait XML
-#
-#     Espera a que se termine de generar el log y despliega status en pantalla
-#
-
-# In[10]:
+# In[8]:
 
 
-def waitXML(firewall, token, job, maxlogs):
+def waitXML(firewall, token, job, maxlogs,timeout=120):
     print('Waiting for XML...')
     progress = 0
     import requests
@@ -79,20 +72,23 @@ def waitXML(firewall, token, job, maxlogs):
         }
 
     status = ''
-    while progress < 100 and status != 'FIN':
+    a = datetime.now() + timedelta(seconds = timeout)
+    while progress < 100 and status != 'FIN' and datetime.now() < a:
         response = requests.request("GET", url, headers=headers, params=querystring,verify=False)
         xml = response.text
-        status = xml.split('<status>')[1].split('</status>')[0]
+        status = xml.split('<status>')[1].split('</status>')[0]    
         progress = int(xml.split('progress="')[1].split('"')[0])
         print('Status:{}%\t{}'.format(progress,status),end='\r')
         time.sleep(3)
+        if datetime.now() > a:
+            print('Timeout Error!')
+            return True
     print('Status:{}%\t{}'.format(progress,status))
     print('Done!')
+    return False
 
 
-# # Get XML
-
-# In[11]:
+# In[9]:
 
 
 def getXML(firewall, token, job, maxlogs):
@@ -113,27 +109,23 @@ def getXML(firewall, token, job, maxlogs):
     response = requests.request("GET", url, headers=headers, params=querystring,verify=False)
 
     xml = response.text
-
+    
     with open(os.path.expanduser('~/NorsePi/XML/LastHour.xml'),'w') as file:
         file.write(xml)
-
+        
     print('Finished.')
 
 
-# # New_Parser.py
-#
-
-# In[39]:
+# In[10]:
 
 
 def xmlParser(file=''):
     """
-        El código toma el archivo recibido de palo alto como xml y lo
-        convierte a un archivo JSON que puede ser leído de manera más
+        El código toma el archivo recibido de palo alto como xml y lo 
+        convierte a un archivo JSON que puede ser leído de manera más 
         sencilla por el programa de mapa de ataques o cualquier otro que
         consuma la info por JS
     """
-    drop_values = set()
     color_code = {'critical':'#ff4660', #red
                   'high':'#f48154',  #orange
                  'medium':'#d9ff7f', #yellow
@@ -154,8 +146,10 @@ def xmlParser(file=''):
     reform = a['logs']['entry']
     df = pd.DataFrame(reform)
     df = df[['direction','device_name','time_generated',"src",'srcloc','dst','dstloc','subtype','threatid','severity']]
+    print('Generating First JSON...')
+    df.to_json('LastHour_1.json',orient='index')
     print('Finished. Moving on...')
-
+    
     df1 = df['device_name'].map(lambda x : x.split('-')[1])
     countries = pd.read_csv(os.path.expanduser('~/NorsePi/CSV/all_countries.csv'),sep='\t',index_col=0)
     Tec = pd.read_csv(os.path.expanduser('~/NorsePi/CSV/GPSTec.csv'))
@@ -167,94 +161,80 @@ def xmlParser(file=''):
     df['dstlong'] = ''
     a = len(df)
     for idx in range(a):
-        print('{}/{}'.format(idx+1,a),end='\r')
+        try:
+            print('{}/{}'.format(idx+1,a),end='\r')
 
-        """
-        Pone el codigo alpha-2 como identificador de source y destiny
-        """
-        df['dstloc'][idx] = df['device_name'][idx].split('-')[1]
-        df['srcloc'][idx] = df['srcloc'][idx]['@cc']
+            """
+            Pone el codigo alpha-2 como identificador de source y destiny
+            """
+            df['dstloc'][idx] = df['device_name'][idx].split('-')[1]
+            df['srcloc'][idx] = df['srcloc'][idx]['@cc']
 
-        """
-        Si viene como rango de IP, cambia por el default ('MX')
+            """
+            Si viene como rango de IP, cambia por el default ('MX')
 
-        Checar aqui las ubicaciones del TEC!!!!
+            Checar aqui las ubicaciones del TEC!!!!
 
-        """
-        if len(df['srcloc'][idx]) > 2:
-            #print('{}->"MX"'.format(df['srcloc'][idx]))
-            df['srcloc'][idx] = default_country ######################################3
-        """
-        Cambia severidad por codigo de colores
-        """
-        df['severity'][idx] = color_code[df['severity'][idx]]
-        """
-        Cambia el nombre del pais de acuerdo al codigo alpha-2
-        """
+            """
+            if len(df['srcloc'][idx]) > 2:
+                #print('{}->"MX"'.format(df['srcloc'][idx]))
+                df['srcloc'][idx] = default_country ######################################3
 
-        tmp = countries[countries['country'] == df['srcloc'][idx]]['name'].reset_index(drop=True)
-        if len(tmp) == 0:
-            print('Pais no encontrado: alpha2={}, indice={}'.format(df['srcloc'][idx],idx))
-            tmp = 'INTERNO'
-            df['srcloc'][idx] = default_country
-        else:
-            tmp = tmp[0]
+            """
+            Cambia severidad por codigo de colores
+            """
+            df['severity'][idx] = color_code[df['severity'][idx]]
 
-        df['srcname'][idx] = tmp
-########################################################3
-        """Ideia: Botar uma flag em caso de que nao seja pais atacado! :D"""
-        tmp = Tec[Tec['Campus'] == df['dstloc'][idx]]['Nombre'] #!!!!!!!!!!!!!!!!!
-        if len(tmp) == 0:
-            print('Campus no encontrado: {}, indice={}'.format(df['dstloc'][idx],idx))
-            tmp = countries[countries['country3'] == df1.iloc[idx]]['name'].reset_index(drop=True)
+            """
+            Cambia el nombre del pais de acuerdo al codigo alpha-2
+            """
+            try:
+                df['srcname'][idx] = countries[countries['country'] == df['srcloc'][idx]]['name'].reset_index(drop=True)[0]
+            except:
+                print('Pais no encontrado: alpha2={}, indice={}'.format(df['srcloc'][idx],idx))
+                df['srcloc'][idx] = default_country
+                df['srcname'][idx] = 'INTERNO'
+
+            """Ideia: Botar uma flag em caso de que nao seja pais atacado! :D"""
+            tmp = Tec[Tec['Campus'] == df['dstloc'][idx]]['Nombre'] #!!!!!!!!!!!!!!!!!
             if len(tmp) == 0:
-                # tmp = default_country
-                drop_values.add(idx)
+                print('Campus no encontrado: {}, indice={}'.format(df['dstloc'][idx],idx))
+                try:
+                    df['dstname'][idx] = countries[countries['country3'] == df1.iloc[idx]]['name'].reset_index(drop=True)[0]
+                except:
+                    df['dstname'][idx] = default_country
             else:
-                tmp = tmp[0]
-        else:
-            tmp = tmp.reset_index(drop=True)[0]
-        df['dstname'][idx] = tmp
-########################################################3
-        """
-        TODO
-        Cambia cordinadas de destino de acuerdo al nombre del dispositivo
-        """
-        tmp = Tec[Tec['Campus'] == df1.iloc[idx]]['longitud']
-        if len(tmp) == 0:
-            tmp = countries[countries['country3'] == df['dstloc'][idx]]['longitude'].reset_index(drop=True)
+                tmp = tmp.reset_index(drop=True)[0]
+            df['dstname'][idx] = tmp
+
+            """
+            TODO
+            Cambia cordinadas de destino de acuerdo al nombre del dispositivo
+            """
+            tmp = Tec[Tec['Campus'] == df1.iloc[idx]]['longitud']
             if len(tmp) == 0:
-                # tmp = default_country
-                drop_values.add(idx)
+                tmp = countries[countries['country3'] == df['dstloc'][idx]]['longitude'].reset_index(drop=True)[0]
             else:
-                tmp = tmp[0]
-        else:
-            tmp = tmp.reset_index(drop=True)[0]
+                tmp = tmp.reset_index(drop=True)[0]
 
-        df['dstlong'][idx] = tmp
-        ################# consertar os splits########################l
-        tmp = Tec[Tec['Campus'] == df1.iloc[idx]]['latitud']
-        if len(tmp) != 0:
-            tmp = tmp.reset_index(drop=True)[0]
-        else:
-            tmp = countries[countries['country3'] == df['dstloc'][idx]]['latitude'].reset_index(drop=True)
-            if len(tmp) == 0:
-                # tmp = default_country
-                drop_values.add(idx)
+            df['dstlong'][idx] = tmp
+            ################# consertar os splits########################l
+            tmp = Tec[Tec['Campus'] == df1.iloc[idx]]['latitud']
+            if len(tmp) != 0:
+                tmp = tmp.reset_index(drop=True)[0]
             else:
-                tmp = tmp[0]
-        df['dstlat'][idx] = tmp
+                tmp = countries[countries['country3'] == df['dstloc'][idx]]['latitude'].reset_index(drop=True)[0]
+            df['dstlat'][idx] = tmp    
 
-        """
-        Cambia cordinadas de fuente
-        """
-        tmp = str(countries[countries['country'] == df['srcloc'][idx]]['longitude']).split()[1]
-        df['srclong'][idx] = tmp
-        tmp = str(countries[countries['country'] == df['srcloc'][idx]]['latitude']).split()[1]
-        df['srclat'][idx] = tmp
-
-    df = df.drop(list(drop_values))
-    df.reset_index(drop=True,inplace=True)
+            """
+            Cambia cordinadas de fuente 
+            """
+            tmp = str(countries[countries['country'] == df['srcloc'][idx]]['longitude']).split()[1]
+            df['srclong'][idx] = tmp
+            tmp = str(countries[countries['country'] == df['srcloc'][idx]]['latitude']).split()[1]
+            df['srclat'][idx] = tmp    
+        except Exception as e:
+            pass
     a = ['device_name',
      'direction',
      'src',
@@ -275,28 +255,73 @@ def xmlParser(file=''):
     df = df.sort_values('time_generated',ascending=False)
     df.reset_index(drop=True,inplace=True)
     df.to_json(os.path.expanduser('~/NorsePi/XML/LastHour.json'),orient='index')
+    
 
-# # Main program
 
-# In[13]:
+# In[19]:
+
+
+def fixTime(df=pd.read_json(os.path.expanduser('~/NorsePi/XML/LastHour.json'),orient='index'),tiempoMin=15):
+    b = df['time_generated']
+    try:
+        c = b.map(lambda x : datetime.strptime(b[0],'%Y/%m/%d %H:%M:%S') + timedelta(minutes = tiempoMin))
+    except:
+        c = b.map(lambda x : datetime.strptime(b[0],'%Y-%m-%d %H:%M:%S') + timedelta(minutes = tiempoMin))
+    df['time_generated'] = c
+    df['time_generated']=df['time_generated'].astype(str)
+    df.to_json(os.path.expanduser('~/NorsePi/XML/LastHour.json'),orient='index')
+    return df
+
+
+# In[22]:
+
+
+def fixTime2(df=pd.read_json(os.path.expanduser('~/NorsePi/XML/LastHour.json'),orient='index'),tiempoMin=15):
+    """
+    Tiempo actual -15 mas valor aleatorio
+    """
+    b = df['time_generated']
+    c = b.map(lambda x : (datetime.now() - timedelta(seconds = round(random.uniform(0, tiempoMin*60)))).strftime('%Y-%m-%d %H:%M:%S'))
+    df['time_generated'] = c
+    df = df.sort_values(['time_generated'])
+    df['time_generated']=df['time_generated'].astype(str)
+    df.to_json(os.path.expanduser('~/NorsePi/XML/LastHour.json'),orient='index')
+    return df
+
+
+# In[20]:
 
 
 if __name__ == '__main__':
-
+    
     urllib3.disable_warnings()
-
+    
     firewall='10.4.29.121'
 
     maxlogs=1000
-    minutes = 15
 
     with open(os.path.expanduser('~/NorsePi/SHELL/.tok.tmp'),'r') as file:
         token = file.read()
 
-    job = getJob(firewall,token,maxlogs,minutes)
+    job = getJob(firewall,token,maxlogs)
 
-    waitXML(firewall,token,job,maxlogs)
+    if waitXML(firewall,token,job,maxlogs,1):
+        ### Send email on error
+        print('error!!')
+        fixTime2()
+    else:
+        getXML(firewall,token,job,maxlogs)
+        xmlParser()
 
-    getXML(firewall,token,job,maxlogs)
 
-    xmlParser()
+# In[17]:
+
+
+xmlParser()
+
+
+# In[ ]:
+
+
+
+
