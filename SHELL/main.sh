3b32756756b9f609afeb84845e18698209f140d6
gogvale <gabriel.oliveira@microplus.com.mx>
2018-10-11 17:30:34 -0500

    finished query functions

--- SHELL/main.sh
+++ SHELL/main.sh
@@ -7,9 +7,6 @@
 ########################################################################
 
 echo "Getting xml file"
-python3 $HOME/NorsePi/SHELL/PaloAltoXML.py
-
-echo "Beautifying JSON"
-cat $HOME/NorsePi/XML/LastHour.json | python -m json.tool | tee $HOME/NorsePi/XML/LastHourReadable.json
+python3 $HOME/NorsePi/SHELL/PaloAltoXML.py && cat $HOME/NorsePi/XML/LastHour.json | python -m json.tool | tee $HOME/NorsePi/XML/LastHourReadable.json
 rm -rf $HOME/.config/chromium/Default/*Cache
 bash $HOME/refreshChrome.sh
