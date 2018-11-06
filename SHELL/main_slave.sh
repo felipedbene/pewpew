5ad153103bafd75d652768e887f51209335dc1f8
gogvale <gabriel.oliveira@microplus.com.mx>
2018-11-06 15:46:53 -0600

    log update conditional

--- SHELL/main_slave.sh
+++ SHELL/main_slave.sh
@@ -11,7 +11,12 @@ python3 $HOME/NorsePi/SHELL/GetFromDB.py tiempo=1440
 echo "Parsing values for dashboard"
 python3 $HOME/NorsePi/SHELL/GetDatamapStatus.py
 echo "Cleaning file"
-cat $HOME/NorsePi/XML/LastHour.json | python3 -m json.tool | tee $HOME/NorsePi/XML/LastHourReadable.json
+teste=$(cat $HOME/NorsePi/XML/LastHour.json);
+if [[ $teste =~ "\{\}" ]]; then #if is empty
+    date | tee -a ~/log.txt 
+else # if there's something on json
+    cat $HOME/NorsePi/XML/LastHour.json | python3 -m json.tool | tee $HOME/NorsePi/XML/LastHourReadable.json
+fi
 echo "Clearing cache"
 rm -rf $HOME/.config/chromium/Default/*Cache*
 echo "Refreshing Browser"
