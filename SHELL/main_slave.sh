af2b048cf0a767d0841bd73bb74f1802eced28dc
gogvale <gabriel.oliveira@microplus.com.mx>
2018-11-07 17:10:24 -0600

    "Fix" carousel

--- SHELL/main_slave.sh
+++ SHELL/main_slave.sh
@@ -11,12 +11,7 @@ python3 $HOME/NorsePi/SHELL/GetFromDB.py tiempo=1440
 echo "Parsing values for dashboard"
 python3 $HOME/NorsePi/SHELL/GetDatamapStatus.py
 echo "Cleaning file"
-teste=$(cat $HOME/NorsePi/XML/LastHour.json);
-if [[ $teste =~ "\{\}" ]]; then #if is empty
-    date | tee -a ~/log.txt 
-else # if there's something on json
-    cat $HOME/NorsePi/XML/LastHour.json | python3 -m json.tool | tee $HOME/NorsePi/XML/LastHourReadable.json
-fi
+cat $HOME/NorsePi/XML/LastHour.json | python3 -m json.tool | tee $HOME/NorsePi/XML/LastHourReadable.json
 echo "Clearing cache"
 rm -rf $HOME/.config/chromium/Default/*Cache*
 echo "Refreshing Browser"
