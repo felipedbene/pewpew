9a424b8543aed57c9ffc54421a86e8095a67de01
gogvale <gabriel.oliveira@microplus.com.mx>
2018-11-22 08:35:22 -0600

    Relative Path on GPSTec.csv

--- SHELL/PaloAltoXML.py
+++ SHELL/PaloAltoXML.py
@@ -16,7 +16,7 @@ import os
 import pandas as pd
 
 
-# In[2]:
+# In[1]:
 
 
 def xmlParser(file=''):
@@ -90,7 +90,7 @@ def xmlParser(file=''):
     campi = pd.read_csv(os.path.expanduser('~/NorsePi/CSV/GPSTec.csv'))
 
     # TODO Ler lista Felipe
-    campusDevice = pd.read_csv(os.path.expanduser('/home/gabriel/NorsePi/CSV/GPSTec.csv'))
+    campusDevice = pd.read_csv(os.path.expanduser('~/NorsePi/CSV/GPSTec.csv'))
 
     notFound = set()
     for idx in range(len(df)):
