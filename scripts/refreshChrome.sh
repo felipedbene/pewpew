b13fb7aa7ceeae6b8375f204ec426edabb1e3794
felipe.debene <felipe.debene@gmail.com>
2018-12-14 16:59:02 -0600

    new scripts folders

---
+++ scripts/refreshChrome.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export DISPLAY=":0"
+rm -rf $HOME/.config/chromium/Default/*Cache*
+WID=$(xdotool search --onlyvisible --class chromium|head -1)
+xdotool windowactivate ${WID}
+xdotool key ctrl+F5
+
