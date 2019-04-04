34edebbd66625b455dc73abf1e931c71115f8fff
felipedbene <felipe.debene@gmail.com>
2019-04-04 15:17:47 -0600

    fixed cache dir

--- scripts/start_slave.sh
+++ scripts/start_slave.sh
@@ -8,4 +8,4 @@
 
 echo "Sleeping 10 secs so docker can start, moves mouse and start browser"
 sleep 10
-xdotool mousemove $(xdpyinfo | awk '/dimensions/{print $2}' | sed -e 's/x/ /g') && chromium-browser --disable-web-security --user-data-dir="~" --kiosk --anonymous --app=http://localhost:3248/Pantalla_1.html
+xdotool mousemove $(xdpyinfo | awk '/dimensions/{print $2}' | sed -e 's/x/ /g') && chromium-browser --disable-web-security --user-data-dir=cache/ --kiosk --anonymous --app=http://localhost:3248/Pantalla_1.html
