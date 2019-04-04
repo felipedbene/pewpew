a7c9dfbc163687f40a8563154799c67e8f3d4e19
felipedbene <felipe.debene@gmail.com>
2019-04-04 11:44:30 -0600

    Fixed Scripts

--- scripts/refreshChrome.sh
+++ scripts/refreshChrome.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export DISPLAY=":0"
 rm -rf $HOME/.config/chromium/Default/*Cache*
+rm -rf $HOME/cache/*
 WID=$(xdotool search --onlyvisible --class chromium|head -1)
 xdotool windowactivate ${WID}
 xdotool key ctrl+F5
---
+++ scripts/start_slave.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+###############################################################################
+# El siguiente programa hace con que se ejecute la descarga de logs a cada 10 #
+# minutos, inicia el servidor local y mueve el ratón a la parte inferior      #
+# izquierda de la pantalla. Debe ser la primera cosa al iniciar después de    #
+# prender.                                                                    #
+###############################################################################
+
+echo "Sleeping 9 secs so docker can start, moves mouse and start browser"
+sleep 9
+xdotool mousemove $(xdpyinfo | awk '/dimensions/{print $2}' | sed -e 's/x/ /g') && chromium-browser --disable-web-security --user-data-dir="~" --kiosk --anonymous --app=http://localhost:3248/Pantalla_1.html
